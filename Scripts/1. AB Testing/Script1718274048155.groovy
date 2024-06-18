d6e527cf20bef86eb862d53cfc82802fd76956e3
noble.moon <adzkiahnab@gmail.com>
2024-06-18 13:34:45 +0700

    - A/B testing make it complicated

--- Scripts/1. AB Testing/Script1718274048155.groovy
+++ Scripts/1. AB Testing/Script1718274048155.groovy
@@ -13,9 +13,35 @@ import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.webui.keyword.internal.WebUIAbstractKeyword as WebUIAbstractKeyword
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//1. direct to the main page
 WebUI.openBrowser(GlobalVariable.urlMain)
 
+//2. check if the button is clickable
+WebUI.verifyElementClickable(findTestObject('Page_The Internet/a_AB Testing'), FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('Object Repository/Page_The Internet/a_AB Testing'), FailureHandling.STOP_ON_FAILURE)
+WebUI.navigateToUrl('https://the-internet.herokuapp.com/abtest')
+
+//4. verify the content
+String expectedTitle = ['A/B Test Controlx', 'A/B Test Variation 1x']
+String actualTitle = WebUI.getText(findTestObject('Object Repository/Page_The Internet/h3_AB Test Control'))
+if (expectedTitle.contains(actualTitle)) println('up')
+	else println('down')
+
+String expectedPar = 'Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through).'
+String actualPar = WebUI.getText(findTestObject('Object Repository/Page_The Internet/p_Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text andor functionality works best towards a desired outcome (e.g. a u'))
+WebUI.verifyEqual(actualPar, expectedPar, FailureHandling.CONTINUE_ON_FAILURE)
+
+//5. Verify Footer
+String expectedFooter = 'Powered by Elemental Selenium'
+String actualFooter = WebUI.getText(findTestObject('Object Repository/Page_The Internet/div_Powered by Elemental Selenium'))
+WebUI.verifyEqual(actualFooter, expectedFooter, FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Page_The Internet/a_Elemental Selenium'), FailureHandling.STOP_ON_FAILURE)
+WebUI.switchToWindowUrl('https://elementalselenium.com/')
+
+WebUI.closeBrowser()
